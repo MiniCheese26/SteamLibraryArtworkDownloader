affaf2bad0e50f771986453455b959d2bad5a8cc
loc0 <minicheese28@protonmail.com>
2019-11-04 18:17:08 +0000

    Initial commit

---
+++ SteamLibraryArtworkDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamLibraryArtworkDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
